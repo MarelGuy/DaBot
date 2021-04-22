ab95300270aad305c7fd266445758f8a0dedb9dc
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2021-04-22 22:14:43 +0200

    added help command

---
+++ src/commands/help.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'help',
+    description: 'Insulti una persona',
+    execute: async (message, args) => {
+        if (args.length === 0) {
+            message.channel.send("- HELP (Prefix: $) -\n\n**You can do $help <command> for further informations**\n- insulta <name> (ale/cris/teo/loris)\n- aggiungi-insulto <name> <insult> (ale/cris/teo/loris)");
+        }
+
+        switch (args[0]) {
+            case "insulta":
+                message.channel.send("- HELP -\n\nUsage: insulta <name>\nThis command needs the name of the person that you want to insult, for now the only persons available are ale, cris, teo and loris.");
+                break;
+            case "aggiungi-insulto":
+                message.channel.send("- HELP -\n\nUsage: aggiungi-insulto <name> <insult>\nYou need the name of which person you want to insult, with of course, an insult.");
+                break;
+        }
+    }
+};
