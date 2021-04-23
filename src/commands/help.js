335f0259778f63dd649e4555b8da6800a611842e
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2021-04-23 19:31:12 +0200

    added every command to help

--- src/commands/help.js
+++ src/commands/help.js
@@ -10,8 +10,20 @@ module.exports = {
             case "insulta":
                 message.channel.send("- HELP -\n\nUsage: insulta <name>\nThis command needs the name of the person that you want to insult, for now the only persons available are ale, cris, teo and loris.");
                 break;
+            case "tts-insulta":
+                message.channel.send("- HELP -\n\nUsage: tts-insulta <name>\nThis command needs the name of the person that you want to insult with the text to speech, for now the only persons available are ale, cris, teo and loris.");
+                break;
             case "aggiungi-insulto":
                 message.channel.send("- HELP -\n\nUsage: aggiungi-insulto <name> <insult>\nYou need the name of which person you want to insult, with of course, an insult.");
+                break;
+            case "overpopulate":
+                message.channel.send("- HELP -\n\nUsage: overpopulate <name>\nYou need the name of which person you want to add to the monologue");
+                break;
+            case "bonk":
+                message.channel.send("- HELP -\n\nUsage: bonk\nbonk");
+                break;
+            case "ping":
+                message.channel.send("- HELP -\n\nUsage: ping\nGet the latency of the bot (ATTENTION: You might go travel trough time!)");
                 break;
         }
     }
