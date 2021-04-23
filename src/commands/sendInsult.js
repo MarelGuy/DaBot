67ee7e06b6b4b45e264b733c6293f9a8936c2277
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2021-04-23 11:45:37 +0200

    trying tts

--- src/commands/sendInsult.js
+++ src/commands/sendInsult.js
@@ -6,7 +6,7 @@ module.exports = {
 
         if (args.length === 1) {
             const insulti = await insultModel.find({ for: args[0].toString() })
-            message.channel.send(insulti[Math.floor(Math.random() * insulti.length)].insulto.toString());
+            message.channel.send(insulti[Math.floor(Math.random() * insulti.length)].insulto.toString(), { tts: true });
         }
         else
             message.channel.send("Must provide just one argument");
