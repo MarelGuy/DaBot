c71b8eb39645d7733db0cf5f0a405bdfefa07a94
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2021-04-29 12:44:27 +0200

    Discontinued

--- src/commands/overpopulation.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'overpopulate',
-    description: 'Insulti su ale',
-    execute: async (message, args) => {
-        message.channel.send(`Non ti è mai venuto in mente che questo pianeta è sovrappopolato? Solo una manciata di umani conta davvero. Tutti gli altri sono solo tanta erbaccia. Quindi ora devo separare questa erbaccia dal grano. E con ${args.join(" ")}, posso finalmente farcela!`);
-    }
-};
