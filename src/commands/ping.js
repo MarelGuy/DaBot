cb2ff4228cc09d2dd19aef0495d137da5486d217
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2021-05-06 13:22:01 +0200

    literally added a pong

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ping',
     description: 'Gives the latency of the server',
     execute: async (message, args) => {
-        message.channel.send(`Latency is ${Date.now() - message.createdTimestamp}ms.`);
+        message.channel.send(`Pong! Latency is ${Date.now() - message.createdTimestamp}ms.`);
     }
 };
