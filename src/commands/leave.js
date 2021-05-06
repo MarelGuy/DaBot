a56c3cf8e6790b9721d2d8fdfbb25da5b819d600
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2021-05-06 12:46:06 +0200

    added channel and user detection

--- src/commands/leave.js
+++ src/commands/leave.js
@@ -1,7 +1,19 @@
 module.exports = {
     name: 'leave',
     description: 'Leave a channel',
-    execute: async (message, args) => {
-        message.member.voice.channel.leave()
+    execute: async (message, args, client) => {
+        const chn = await message.member.voice.channel;
+        if (chn === null) {
+            message.channel.send("You are not in a voice channel!")
+        } else {
+            if (await client.voice.connections.some((connection) => connection.channel.id === message.member.voice.channelID))
+                await chn.leave()
+            else {
+                if (await client.voice.connections.some((connection) => connection.channel.id))
+                    message.channel.send('You need to be in the same channel as the bot!')
+                else
+                    message.channel.send("I'm not in a voice channel!")
+            }
+        }
     }
-};
+}
